7a3f55a2e084164c2469e18914537c432c8bb026
Danyil <dkichalo@gmail.com>
2020-01-23 21:14:28 +0200

    Updates

---
+++ precache-manifest.253261f680cc44c010d648e40672eedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b036488c128ecb1ea2718e3b7ab651a2",
+    "url": "/tic-tac-toe-app/index.html"
+  },
+  {
+    "revision": "937db1f8616174538781",
+    "url": "/tic-tac-toe-app/static/css/main.74531102.chunk.css"
+  },
+  {
+    "revision": "9f64e0e84d1c0b04d9b9",
+    "url": "/tic-tac-toe-app/static/js/2.e215959a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe-app/static/js/2.e215959a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "937db1f8616174538781",
+    "url": "/tic-tac-toe-app/static/js/main.8e1d09ae.chunk.js"
+  },
+  {
+    "revision": "b45fe2b6ea2df9c61b69",
+    "url": "/tic-tac-toe-app/static/js/runtime-main.4eb552e7.js"
+  }
+]);
